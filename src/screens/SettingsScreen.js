6a96dd3910ef929cb2233baa6019e20771c7bd7d
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-02-11 07:31:02 +0100

    Added basic settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,28 +1,140 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  Image,
+  Platform,
+  RefreshControl,
+  Switch,
+  TouchableHighlight,
+  Picker
+} from 'react-native'
+import { SettingsScreen as SettingsScreenComponent } from 'react-native-settings-screen'
 
+{/* <Picker
+          selectedValue={this.state.language}
+          style={{alignSelf: 'flex-end'}}
+          onValueChange={(itemValue, itemIndex) =>
+            this.setState({language: itemValue})
+          }>
+          <Picker.Item label="English (US)" value="en-US" />
+          <Picker.Item label="English (UK)" value="en-UK" />
+        </Picker> */}
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
     title: 'Settings'
   };
 
+  state = {
+    language: 'en-US'
+  }
+
+  handleOnPressRow = (event) => {
+    alert('Changing this setting becomes available in later versions.')
+  }
+
+  settingsData = [
+    {
+      type: 'SECTION',
+      header: 'Voice'.toUpperCase(),
+      footer:
+        'Changing the voice settings becomes available in later versions.',
+      rows: [
+        {
+          title: 'Language',
+          renderAccessory: () => (
+            <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+              English
+            </Text>
+          ),
+          onPress: this.handleOnPressRow
+        },
+        {
+          title: 'Accent',
+          renderAccessory: () => (
+            <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+              American
+            </Text>
+          ),
+          onPress: this.handleOnPressRow
+        },
+        {
+          title: 'Gender',
+          renderAccessory: () => (
+            <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+              Male
+            </Text>
+          ),
+          onPress: this.handleOnPressRow
+        },
+        {
+          title: 'Speed',
+          renderAccessory: () => (
+            <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+              Normal
+            </Text>
+          ),
+          onPress: this.handleOnPressRow
+        }
+      ],
+    },
+    {
+      type: 'SECTION',
+      header: 'Audio'.toUpperCase(),
+      footer:
+        'Changing the audio settings becomes available in later versions.',
+      rows: [
+        {
+          title: 'Quality',
+          renderAccessory: () => (
+            <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+              Normal
+            </Text>
+          ),
+          onPress: this.handleOnPressRow
+        }
+      ],
+    },
+    {
+      type: 'SECTION',
+      header: 'Advanced'.toUpperCase(),
+      footer:
+        'Changing the advanced settings becomes available in later versions.',
+      rows: [
+        {
+          title: 'Clear cache',
+          renderAccessory: () => (
+            <Text style={{ color: '#999', marginRight: 6, fontSize: 18 }}>
+              50.5MB
+            </Text>
+          ),
+          onPress: this.handleOnPressRow
+        },
+      ],
+    },
+    {
+      type: 'CUSTOM_VIEW',
+      render: () => (
+        <Text
+          style={{
+            alignSelf: 'center',
+            fontSize: 18,
+            color: '#999',
+            marginBottom: 40,
+            marginTop: -30,
+          }}
+        >
+          v1.2.3
+        </Text>
+      ),
+    },
+  ]
+
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Adjust voice settings</Text>
-        <Text>Adjust audio quality</Text>
-        <Text>Version info</Text>
-        <Text>Contact info</Text>
-        <Text>Clear cache</Text>
-      </ScrollView>
+      <SettingsScreenComponent data={this.settingsData} style={{paddingTop: 14}} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  },
-});
