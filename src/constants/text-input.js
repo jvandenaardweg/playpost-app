7a35c17511986f6b4b2e90742f2938d0d0e770db
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-05 18:34:35 +0100

    Make input fields same height

--- src/constants/text-input.js
+++ src/constants/text-input.js
@@ -1,13 +1,13 @@
 import colors from '@/constants/colors';
 
 export default {
-  height: 50,
+  height: 55,
   backgroundColor: colors.grayLightest,
   borderWidth: 0,
   borderRadius: 4,
   paddingLeft: 12,
   paddingRight: 12,
   marginBottom: 12,
-  fontSize: 16,
+  fontSize: 17,
   // backgroundColor: 'white'
 };
