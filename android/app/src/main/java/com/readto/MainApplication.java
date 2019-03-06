958d3e2f79d31ac91eadbbf6eef2ff0086259274
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-06 12:54:47 +0100

    Android react native restart depedencies

--- android/app/src/main/java/com/readto/MainApplication.java
+++ android/app/src/main/java/com/readto/MainApplication.java
@@ -3,6 +3,7 @@ package com.readto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
 import com.alinz.parkerdan.shareextension.SharePackage;
 import com.guichaguri.trackplayer.TrackPlayer;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeRestartPackage(),
             new SharePackage(),
             new TrackPlayer(),
             new VectorIconsPackage(),
