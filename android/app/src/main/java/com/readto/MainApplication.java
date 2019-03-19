f627675709b8398bf2f4aa82c7ade27594ef4ffd
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-19 09:18:29 +0100

    Android changes for react-native-video

--- android/app/src/main/java/com/readto/MainApplication.java
+++ android/app/src/main/java/com/readto/MainApplication.java
@@ -3,6 +3,7 @@ package com.readto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.brentvatne.react.ReactVideoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rnfs.RNFSPackage;
 import com.reactnativecommunity.netinfo.NetInfoPackage;
@@ -36,6 +37,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactVideoPackage(),
             new RNFetchBlobPackage(),
             new RNFSPackage(),
             new NetInfoPackage(),
