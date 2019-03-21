03ff08085367191604968a93ee8fdc50f48ac04b
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-21 14:10:17 +0100

    Reducer to getArticle, but does not seem to work properly with updating components

--- src/reducers/playlists.ts
+++ src/reducers/playlists.ts
@@ -5,6 +5,10 @@ export const GET_PLAYLISTS = 'playlists/GET_PLAYLISTS';
 export const GET_PLAYLISTS_SUCCESS = 'playlists/GET_PLAYLISTS_SUCCESS';
 export const GET_PLAYLISTS_FAIL = 'playlists/GET_PLAYLISTS_FAIL';
 
+export const GET_ARTICLE = 'playlists/GET_ARTICLE';
+export const GET_ARTICLE_SUCCESS = 'playlists/GET_ARTICLE_SUCCESS';
+export const GET_ARTICLE_FAIL = 'playlists/GET_ARTICLE_FAIL';
+
 export const CREATE_PLAYLIST_ARTICLE = 'playlists/CREATE_PLAYLIST_ARTICLE';
 export const CREATE_PLAYLIST_ARTICLE_SUCCESS = 'playlists/CREATE_PLAYLIST_ARTICLE_SUCCESS';
 export const CREATE_PLAYLIST_ARTICLE_FAIL = 'playlists/CREATE_PLAYLIST_ARTICLE_FAIL';
@@ -18,6 +22,7 @@ export const RESET_STATE = 'playlists/RESET_STATE';
 const GET_PLAYLISTS_FAIL_MESSAGE = 'An unknown error happened while getting your playlist. Please contact us when this happens all the time.';
 const CREATE_PLAYLIST_ARTICLE_FAIL_MESSAGE = 'An unknown error happened while adding this article to your playlist. Please contact us when this happens all the time.';
 const REMOVE_PLAYLIST_ARTICLE_FAIL_MESSAGE = 'An unknown error happened while removing this article from your playlist. Please contact us when this happens all the time.';
+const GET_ARTICLE_FAIL_MESSAGE = 'An unknown error happened while fetching an article. Please contact us when this happens all the time.';
 
 export interface PlaylistsState {
   isLoading: boolean;
@@ -66,6 +71,51 @@ export function playlistsReducer(state = initialState, action: PlaylistActionTyp
         ...state,
         isLoading: false,
         error: (action.error.response) ? action.error.response.data.message : GET_PLAYLISTS_FAIL_MESSAGE
+      };
+
+    case GET_ARTICLE:
+      return {
+        ...state,
+        isLoading: true
+      };
+
+    case GET_ARTICLE_SUCCESS:
+      Analytics.trackEvent('Get article success');
+
+      const article = action.payload.data;
+
+      // Find the default playlist so we can replace the article
+      const updatedPlaylists = state.playlists.map((playlist) => {
+        if (playlist.name === 'Default') {
+          playlist.playlistItems.map((playlistItem) => {
+            if (playlistItem.article.id !== article.id) return playlistItem;
+
+            // Replace the article
+            return playlistItem.article = article;
+          });
+        }
+
+        return playlist;
+      });
+
+      return {
+        ...state,
+        isLoading: false,
+        playlists: updatedPlaylists,
+        error: ''
+      };
+
+    case GET_ARTICLE_FAIL:
+      if (action.error.response && action.error.response.data && action.error.response.data.message) {
+        Analytics.trackEvent('Error get article', { message: action.error.response.data.message });
+      } else {
+        Analytics.trackEvent('Error get article', { message: GET_ARTICLE_FAIL_MESSAGE });
+      }
+
+      return {
+        ...state,
+        isLoading: false,
+        error: (action.error.response) ? action.error.response.data.message : GET_ARTICLE_FAIL_MESSAGE
       };
 
     case RESET_STATE:
@@ -147,6 +197,18 @@ export function getPlaylists() {
       request: {
         method: 'get',
         url: '/v1/me/playlists'
+      }
+    }
+  };
+}
+
+export function getArticle(articleId: string) {
+  return {
+    type: GET_ARTICLE,
+    payload: {
+      request: {
+        method: 'get',
+        url: `/v1/articles/${articleId}`
       }
     }
   };
