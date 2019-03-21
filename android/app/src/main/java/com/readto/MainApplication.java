202c47e6380830b0c04a96147fc1ba814a722e19
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-21 13:19:21 +0100

    Added react native version number

--- android/app/src/main/java/com/readto/MainApplication.java
+++ android/app/src/main/java/com/readto/MainApplication.java
@@ -3,6 +3,7 @@ package com.readto;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.apsl.versionnumber.RNVersionNumberPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.rnfs.RNFSPackage;
@@ -37,6 +38,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNVersionNumberPackage(),
             new ReactVideoPackage(),
             new RNFetchBlobPackage(),
             new RNFSPackage(),
