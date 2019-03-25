9964330406e02b88b8e42990af6144a7b3adc1cf
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-25 10:02:30 +0100

    Update to browser

--- src/screens/BrowserScreen.tsx
+++ src/screens/BrowserScreen.tsx
@@ -1,15 +1,22 @@
 // @ts-ignore
 import React from 'react';
+import { View, Animated } from 'react-native';
 import { NavigationScreenProp, NavigationRoute } from 'react-navigation';
 import { WebView } from 'react-native-webview';
 
 import { ButtonReload } from '../components/Header';
 
+import colors from '../constants/colors';
+
 interface Props {
   navigation: NavigationScreenProp<NavigationRoute>;
 }
 
-export class BrowserScreen extends React.PureComponent<Props> {
+interface State {
+  loadProgress: string;
+}
+
+export class BrowserScreen extends React.PureComponent<Props, State> {
 
   /* tslint:disable-next-line no-any */
   private webviewRef = React.createRef<any>();
@@ -21,24 +28,52 @@ export class BrowserScreen extends React.PureComponent<Props> {
     };
   }
 
+  widthAnimation = new Animated.Value(0);
+
+  state = {
+    loadProgress: ''
+  };
+
   componentDidMount() {
     this.props.navigation.setParams({ handleOnReload: this.handleOnReload });
   }
 
   handleOnReload = () => {
+    this.widthAnimation = new Animated.Value(0);
     this.webviewRef.current && this.webviewRef.current.reload();
+  }
+
+  handleOnLoadProgress = (event: any) => {
+    console.log(event.nativeEvent.progress);
+    Animated.timing(
+      this.widthAnimation,
+      {
+        toValue: event.nativeEvent.progress,
+        duration: 200
+      }
+    ).start();
   }
 
   render() {
     const url = this.props.navigation.getParam('url', null);
 
+    const animatedWidth = this.widthAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0%', '100%'],
+    });
+
     return (
-      <WebView
-        ref={this.webviewRef}
-        source={{ uri: url }}
-        startInLoadingState={true}
-        useWebKit={true}
-       />
+      <View style={{ flex: 1 }}>
+        <Animated.View style={{ position: 'absolute', top: 0, left: 0, width: animatedWidth, backgroundColor: colors.tintColor, height: 5 }}></Animated.View>
+        <WebView
+          ref={this.webviewRef}
+          source={{ uri: url }}
+          // startInLoadingState={false}
+          // onLoadProgress={this.handleOnLoadProgress}
+          // useWebKit={true}
+        />
+      </View>
+
     );
   }
 }
