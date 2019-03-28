df0af60d2319cb41f93660caaf9294ca9e251912
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-28 15:45:10 +0100

    Basic jest testing setup

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "setupFiles": [
-        "<rootDir>/node_modules/appcenter/test/AppCenterMock.js",
-        "<rootDir>/node_modules/appcenter-analytics/test/AppCenterAnalyticsMock.js",
-        "<rootDir>/node_modules/appcenter-crashes/test/AppCenterCrashesMock.js",
-        "<rootDir>/node_modules/appcenter-push/test/AppCenterPushMock.js"
-     ]
-}
