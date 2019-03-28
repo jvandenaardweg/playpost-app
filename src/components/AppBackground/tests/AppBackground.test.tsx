d2778cf96dbc1cf652f9e37a5f6c0552418b9221
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-03-28 15:45:19 +0100

    Added AppBackground tests

---
+++ src/components/AppBackground/tests/AppBackground.test.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, RenderAPI } from 'react-native-testing-library';
+import { AppBackground } from '../AppBackground';
+
+import { Text } from 'react-native';
+
+describe('AppBackground', () => {
+
+  describe('rendering', () => {
+    let wrapper: RenderAPI;
+
+    beforeAll(() => {
+      wrapper = render(<AppBackground><Text>Example!</Text></AppBackground>);
+    });
+
+    it('should render correctly', () => {
+      expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+
+    it('should render a child element', () => {
+      expect(wrapper.getByType(Text).props.children).toBe('Example!');
+    });
+  });
+});
