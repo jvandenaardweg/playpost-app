c66ecbb33c5fe0c3d60ecfa982b381a637f48bdf
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-04-24 15:47:33 +0200

    Made upgrade window more fancy

--- src/components/UpgradeFeatures/UpgradeFeatures.tsx
+++ src/components/UpgradeFeatures/UpgradeFeatures.tsx
@@ -4,6 +4,7 @@ import { Button } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
 import styles from './styles';
+import { PlayButton } from '../Article/Article';
 
 export class UpgradeFeatures extends React.PureComponent {
 
@@ -11,37 +12,50 @@ export class UpgradeFeatures extends React.PureComponent {
     Alert.alert('Upgrade to Pro', 'This is currently not working in this version of the App. Upgrading to Pro becomes available in later versions.');
   }
 
+  handleOnPressRestore = () => {
+    Alert.alert('Restore purchase', 'This is currently not working in this version of the App. Restoring purchases becomes available in later versions.');
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        {/* <View>
-          <Text>Upgrade for premium features</Text>
-        </View> */}
-        <Icon name="star" solid size={32} color="gold" />
-        <View style={styles.feature}>
-          <Text style={styles.title}>Higher quality voices</Text>
-          <Text style={styles.paragraph}>For our pro users we have even higher quality voices that sound more life-like.</Text>
-          <View style={{ flex: 1, flexDirection: 'row' }}>
-            <View><Icon name="play" solid size={24} /></View>
-            <View><Icon name="play" solid size={24} /></View>
-            <View><Icon name="play" solid size={24} /></View>
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Upgrade for premium features</Text>
+
+          <View style={styles.feature}>
+            <Icon name="gem" size={32} style={styles.featureIcon} />
+            <View style={styles.featureContent}>
+              <Text style={styles.title}>Higher quality voices</Text>
+              <Text style={styles.paragraph}>For our pro users we have even higher quality voices that sound more life-like.</Text>
+            </View>
+          </View>
+          <View style={styles.feature}>
+            <Icon name="assistive-listening-systems" solid size={32} style={styles.featureIcon} />
+            <View style={styles.featureContent}>
+              <Text style={styles.title}>Voice customization</Text>
+              <Text style={styles.paragraph}>Choose between a variety of high quality male and female voices with accents like American, British or Australian English.</Text>
+            </View>
+          </View>
+          <View style={styles.feature}>
+            <Icon name="clock" size={32} style={styles.featureIcon} />
+            <View style={styles.featureContent}>
+              <Text style={styles.title}>Unlimited listening</Text>
+              <Text style={styles.paragraph}>No article limits. Listen to articles longer then 10 minutes.</Text>
+            </View>
           </View>
         </View>
-        <View style={styles.feature}>
-          <Text style={styles.title}>Voice customization</Text>
-          <Text style={styles.paragraph}>Choose between a variety of high quality male and female voices with accents like American, British or Australian English.</Text>
-        </View>
-        <View style={styles.feature}>
-          <Text style={styles.title}>Unlimited listening</Text>
-          <Text style={styles.paragraph}>No article limits. Listen to articles longer then 10 minutes.</Text>
-        </View>
+
         <View style={styles.subscribeContainer}>
           <Button title="Upgrade for €9,99 per month" onPress={() => this.handleOnPressUpgrade()} />
+          <Button type="clear" title="Already upgraded? Restore purchase" onPress={() => this.handleOnPressRestore()} titleStyle={{ fontSize: 14 }} />
         </View>
         <View style={styles.footer}>
           <Text style={styles.footerText}>Payment will be charged to your Apple ID account at the confirmation of purchase. Subscription automatically renews unless it is canceled at least 24 hours before the end of the current period. Your account will be charged for renewal within 24 hours prior to the end of the current period. You can manage and cancel your subscriptions by going to your account settings on the App Store after purchase.</Text>
-          <Text style={[styles.footerText, styles.textHighlight]} onPress={() => Linking.openURL('https://playpost.app/privacy-policy?ref=playpost://upgrade')}>Privacy Policy</Text>
-          <Text style={[styles.footerText, styles.textHighlight]} onPress={() => Linking.openURL('https://playpost.app/terms-of-use?ref=playpost://upgrade')}>Terms of Use</Text>
+          <View style={styles.footerLinks}>
+            <Text style={[styles.footerText, styles.textHighlight]} onPress={() => Linking.openURL('https://playpost.app/privacy-policy?ref=playpost://upgrade')}>Privacy Policy</Text>
+            <Text> - </Text>
+            <Text style={[styles.footerText, styles.textHighlight]} onPress={() => Linking.openURL('https://playpost.app/terms-of-use?ref=playpost://upgrade')}>Terms of Use</Text>
+          </View>
         </View>
       </View>
     )
--- src/components/UpgradeFeatures/styles.tsx
+++ src/components/UpgradeFeatures/styles.tsx
@@ -9,36 +9,74 @@ export default StyleSheet.create({
     flex: 1,
     padding: 14,
   },
+  header: {
+    marginBottom: 14
+  },
+  headerTitle: {
+    textAlign: 'center',
+    fontSize: 22,
+    color: colors.paragraphDefault,
+    fontWeight: '600',
+    marginBottom: 24,
+    marginTop: 12
+  },
   title: {
     fontSize: 17,
-    fontWeight: 'bold',
-    color: colors.black,
+    fontWeight: '600',
     marginBottom: 4
   },
   paragraph: {
     color: colors.paragraphGrayed,
-    lineHeight: 22,
-    fontSize: 14
+    lineHeight: 23,
+    fontSize: 15
   },
   feature: {
-    marginBottom: 16
+    marginTop: 14,
+    paddingBottom: 14,
+    borderBottomWidth: 1,
+    borderColor: 'rgba(0,0,0,0.05)',
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  featureContent: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  featureIcon: {
+    color: colors.tintColor,
+    marginRight: 18,
+    marginLeft: 8,
+    width: 40
   },
   footer: {
     marginTop: 16,
     paddingTop: 16,
     borderTopWidth: 1,
     borderColor: colors.grayLightest,
+    paddingBottom: 24
   },
   footerText: {
     fontSize: 12,
     lineHeight: 18,
     opacity: 0.5
   },
+  footerLinks: {
+    flex: 1,
+    flexDirection: 'row',
+    marginTop: 6,
+    justifyContent: 'center'
+  },
   textHighlight: {
     color: colors.black,
     opacity: 1
   },
   subscribeContainer: {
-
+    paddingTop: 24,
+    // borderTopWidth: 1,
+    // borderColor: colors.grayLightest,
+  },
+  icon: {
+    color: colors.tintColor
   }
 });
