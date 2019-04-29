fbfe2d64102579ef5482d591c45e4a776325215d
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-04-29 11:59:34 +0200

    Pass down upgrade handling to voice selection component

--- src/screens/SettingsVoicesScreen.tsx
+++ src/screens/SettingsVoicesScreen.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { NavigationScreenProp, NavigationRoute, NavigationScreenOptions } from 'react-navigation';
+import { NavigationScreenProp, NavigationRoute } from 'react-navigation';
 import { ScrollView } from 'react-native';
 import { VoicesSelect } from '../components/VoicesSelect';
+import { ButtonUpgrade } from '../components/Header/ButtonUpgrade';
 
 interface Props {
   navigation: NavigationScreenProp<NavigationRoute>;
 }
 
 export class SettingsVoicesScreen extends React.PureComponent<Props> {
-  static navigationOptions = ({ navigation }: { navigation: NavigationScreenProp<NavigationRoute> }): NavigationScreenOptions => {
+  static navigationOptions = ({ navigation }: { navigation: NavigationScreenProp<NavigationRoute> }) => {
     return {
       title: 'Voices',
-      // headerLeft: null,
+      headerRight: <ButtonUpgrade onPress={navigation.getParam('handleOnPressUpgrade')} />
     };
+  }
+
+  componentDidMount() {
+    this.props.navigation.setParams({ handleOnPressUpgrade: this.handleOnPressUpgrade });
+  }
+
+  handleOnPressUpgrade = () => {
+    this.props.navigation.navigate('Upgrade');
   }
 
   render() {
     return (
       <ScrollView>
-        <VoicesSelect />
+        <VoicesSelect onPressUpgrade={this.handleOnPressUpgrade} />
       </ScrollView>
     );
   }
