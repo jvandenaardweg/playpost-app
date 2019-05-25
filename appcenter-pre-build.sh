3bfca8715b09efb45de25ae4aa558c6f3ccff007
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-05-25 13:54:25 +0200

    Use pre build to set environments

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# Creates an .env from ENV variables for use with react-native-config
+# Taken from: https://github.com/luggit/react-native-config/issues/282#issuecomment-441615641
+if [ $ENVIRONMENT = "production" ]; then
+   cp .env.prod .env
+elif [ $ENVIRONMENT = "staging" ]; then
+   cp .env.staging .env
+fi
+
+printf "\n.env created with contents:\n"
+cat .env
+
 # Run tslint before running the build
 npm run lint
 
