7a7c36126060d69db46d0b36e694ed94433a483a
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-05-26 00:22:26 +0200

    Cleanup App

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,6 @@ import { reactNativeElementsTheme } from './theme';
 import { AppNavigator } from './navigation/AppNavigator';
 import { NetworkProvider } from './contexts/NetworkProvider';
 import { AppStateProvider } from './contexts/AppStateProvider';
-// import { AppStatus } from './components/AppStatus';
 
 // import { whyDidYouUpdate } from 'why-did-you-update';
 // whyDidYouUpdate(React, { exclude: /^YellowBox|Icon|Swipeable/ });
@@ -25,17 +24,7 @@ console.disableYellowBox = true;
 export default class App extends React.PureComponent {
   componentDidMount() {
     this.setAnalytics();
-
-    // TODO: only when logged in
-    // this.fetchLanguages();
-    // this.fetchUser();
-
-    // AppState.addEventListener('change', this.handleAppStateChange);
   }
-
-  // componentWillUnmount() {
-  //   AppState.removeEventListener('change', this.handleAppStateChange);
-  // }
 
   setAnalytics = async () => {
     if (!__DEV__) {
@@ -43,34 +32,6 @@ export default class App extends React.PureComponent {
       await Analytics.setEnabled(true);
     }
   }
-
-  // handleAppStateChange = async (nextAppState: AppStateStatus) => {
-  //   if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
-  //     // Only fetch the playlist when there's an active internet connection
-  //     const isConnected = await NetInfo.isConnected.fetch();
-
-  //     // TODO: only when logged in
-  //     // if (isConnected) {
-  //     //   this.fetchPlaylist();
-  //     //   this.fetchLanguages();
-  //     //   this.fetchUser();
-  //     // }
-  //   }
-
-  //   this.setState({ appState: nextAppState });
-  // }
-
-  // async fetchPlaylist() {
-  //   store.dispatch(getPlaylist());
-  // }
-
-  // async fetchLanguages() {
-  //   store.dispatch(getLanguages());
-  // }
-
-  // async fetchUser() {
-  //   store.dispatch(getUser());
-  // }
 
   render() {
     return (
