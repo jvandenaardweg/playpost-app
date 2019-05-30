f3c59fc51b9970040137ab3d6d3bc02ede243ab9
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-05-30 11:33:50 +0200

    Added comments to AppDelegate.m

--- ios/Playpost/AppDelegate.m
+++ ios/Playpost/AppDelegate.m
@@ -14,12 +14,15 @@
 #import <React/RCTRootView.h>
 #import "RNSplashScreen.h"
 
+// Custom added
 // iOS 9.x or newer
 // https://facebook.github.io/react-native/docs/linking
 #import <React/RCTLinkingManager.h>
+// /Custom added
 
 @implementation AppDelegate
 
+// Custom added
 // https://facebook.github.io/react-native/docs/linking
 - (BOOL)application:(UIApplication *)application
             openURL:(NSURL *)url
@@ -27,21 +30,27 @@
 {
   return [RCTLinkingManager application:application openURL:url options:options];
 }
+// /Custom added
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
 
-  // Custom added: AppCenter
+  // Custom added
   [AppCenterReactNativeCrashes registerWithAutomaticProcessing];
   [AppCenterReactNativeAnalytics registerWithInitiallyEnabled:false];
   [AppCenterReactNative register];
+  // /Custom added
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Playpost"
                                             initialProperties:nil];
 
+  // Custom added
+  // Original:
+  // rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
   rootView.backgroundColor = [UIColor blackColor];
+  // /Custom added
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
@@ -49,7 +58,9 @@
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
 
+  // Custom added
   [RNSplashScreen show];
+  // /Custom added
 
   return YES;
 }
@@ -58,17 +69,18 @@
 {
 #if DEBUG
 
-  // Custom added: This prevents the logs from constantly showing websocket connection errors
+  // Custom added
+  // This prevents the logs from constantly showing websocket connection errors
   // https://github.com/facebook/react-native/issues/21030#issuecomment-493392051
   // original:
   // return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   NSURL *jsUrl = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
   [[RCTBundleURLProvider sharedSettings] setJsLocation:jsUrl.host];
   return jsUrl;
+  // /Custom added
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
 @end
-
