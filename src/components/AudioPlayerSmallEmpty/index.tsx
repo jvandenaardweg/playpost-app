a48eb4ee518cabf3642c20df5c83c815fd1d5ac9
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-06-13 10:29:32 +0200

    Remove icon from small audio player empty

--- src/components/AudioPlayerSmallEmpty/index.tsx
+++ src/components/AudioPlayerSmallEmpty/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import * as Icon from '../../components/Icon';
+// import * as Icon from '../../components/Icon';
 
 import styles from './styles';
 
@@ -10,9 +10,9 @@ export const AudioPlayerSmallEmpty: React.FC = React.memo(() => (
     <View style={styles.container}>
       <View style={styles.placeHolder}>
         <Text style={styles.emptyText}>Select an article to listen</Text>
-        <View style={styles.icon}>
+        {/* <View style={styles.icon}>
           <Icon.FontAwesome name="arrow-up" color="white" size={14} />
-        </View>
+        </View> */}
       </View>
     </View>
   </View>
