36183a116935f5f1a8b67f22a79afb6f85482049
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-01 16:35:28 +0200

    Updated LoginForm tests

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -12,28 +12,22 @@ interface Props {
   isLoading: boolean;
 }
 
-export const LoginForm: React.FC<Props> = React.memo(({
-  email,
-  password,
-  isLoading,
-  onChangeText,
-  onPressLogin,
-  onPressForgotPassword
-}) => {
-
+export const LoginForm: React.FC<Props> = React.memo(({ email, password, isLoading, onChangeText, onPressLogin, onPressForgotPassword }) => {
   let passwordInput: TextInput | null = null;
 
   return (
-    <KeyboardAvoidingView testID="login-form" style={styles.container} behavior="padding" enabled>
+    <KeyboardAvoidingView testID="LoginForm" style={styles.container} behavior="padding" enabled>
       <ScrollView style={styles.form} contentContainerStyle={styles.formContent} keyboardShouldPersistTaps={'handled'}>
-
         <TextInput
+          testID="LoginForm-TextInput-email"
           placeholder="E-mail address"
           autoCapitalize="none"
           value={email}
           onChangeText={text => onChangeText('email', text)}
           textContentType="username"
-          onSubmitEditing={() => { passwordInput && passwordInput.focus(); }}
+          onSubmitEditing={() => {
+            passwordInput && passwordInput.focus();
+          }}
           style={styles.textField}
           keyboardType="email-address"
           returnKeyType="next"
@@ -43,7 +37,10 @@ export const LoginForm: React.FC<Props> = React.memo(({
         />
 
         <TextInput
-          ref={(input) => { passwordInput = input; }}
+          testID="LoginForm-TextInput-password"
+          ref={input => {
+            passwordInput = input;
+          }}
           placeholder="Password"
           autoCapitalize="none"
           secureTextEntry
@@ -58,10 +55,19 @@ export const LoginForm: React.FC<Props> = React.memo(({
         />
 
         <View>
-          <Button title="Login" loading={isLoading} onPress={onPressLogin} disabled={isLoading} buttonStyle={styles.buttonStyle} disabledStyle={styles.buttonStyle} activeOpacity={1} titleStyle={styles.buttonTitleStyle} />
-          <Button title="Forgot password?" type="clear" onPress={onPressForgotPassword} />
+          <Button
+            testID="LoginForm-Button-login"
+            title="Login"
+            loading={isLoading}
+            onPress={onPressLogin}
+            disabled={isLoading}
+            buttonStyle={styles.buttonStyle}
+            disabledStyle={styles.buttonStyle}
+            activeOpacity={1}
+            titleStyle={styles.buttonTitleStyle}
+          />
+          <Button testID="LoginForm-Button-forgot-password" title="Forgot password?" type="clear" onPress={onPressForgotPassword} />
         </View>
-
       </ScrollView>
     </KeyboardAvoidingView>
   );
--- src/components/LoginForm/tests/index.test.tsx
+++ src/components/LoginForm/tests/index.test.tsx
@@ -1,19 +1,91 @@
 import React from 'react';
-import { render, RenderAPI } from 'react-native-testing-library';
+import { render, RenderAPI, fireEvent } from 'react-native-testing-library';
 
 import { LoginForm } from '../index';
 
 describe('LoginForm', () => {
-
   describe('rendering', () => {
     let wrapper: RenderAPI;
+    const onPressLoginHandler = jest.fn();
+    const onChangeTextHandler = jest.fn();
+    const onPressForgotPasswordHandler = jest.fn();
 
-    beforeAll(() => {
-      wrapper = render(<LoginForm onChangeText={() => {}} onPressLogin={() => {}} onPressForgotPassword={() => {}} email="" password="" isLoading={false} />);
+    beforeEach(() => {
+      wrapper = render(
+        <LoginForm
+          onChangeText={onChangeTextHandler}
+          onPressLogin={onPressLoginHandler}
+          onPressForgotPassword={onPressForgotPasswordHandler}
+          email=""
+          password=""
+          isLoading={false}
+        />
+      );
     });
 
     it('should render correctly', () => {
       expect(wrapper.toJSON()).toMatchSnapshot();
+    });
+  });
+
+  describe('interacting', () => {
+    let wrapper: RenderAPI;
+    const onPressLoginHandler = jest.fn();
+    const onChangeTextHandler = jest.fn();
+    const onPressForgotPasswordHandler = jest.fn();
+
+    beforeEach(() => {
+      wrapper = render(
+        <LoginForm
+          onChangeText={onChangeTextHandler}
+          onPressLogin={onPressLoginHandler}
+          onPressForgotPassword={onPressForgotPasswordHandler}
+          email=""
+          password=""
+          isLoading={false}
+        />
+      );
+    });
+
+    it('should fire onPressLogin when the login button is pressed', () => {
+      fireEvent.press(wrapper.getByTestId('LoginForm-Button-login'));
+      expect(onPressLoginHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it('should fire onPressForgotPassword when the forgot password button is pressed', () => {
+      fireEvent.press(wrapper.getByTestId('LoginForm-Button-forgot-password'));
+      expect(onPressForgotPasswordHandler).toHaveBeenCalledTimes(1);
+    });
+
+    it('should fire onChangeText when filling in a TextInput', () => {
+      const exampleEmail = 'jordyvandenaardweg@gmail.com';
+      const emailTextInput = wrapper.getByTestId('LoginForm-TextInput-email');
+
+      fireEvent.changeText(emailTextInput, exampleEmail);
+      expect(onChangeTextHandler).toHaveBeenCalledWith('email', exampleEmail);
+    });
+
+    it('should fire onChangeTextHandler when filling in a TextInput', () => {
+      const examplePassword = 'p@ssword!';
+      const passwordTextInput = wrapper.getByTestId('LoginForm-TextInput-password');
+
+      fireEvent.changeText(passwordTextInput, examplePassword);
+      expect(onChangeTextHandler).toHaveBeenCalledWith('password', examplePassword);
+    });
+
+    it('should disable the signup button if isLoading is true', () => {
+      wrapper.update(
+        <LoginForm
+          onChangeText={onChangeTextHandler}
+          onPressLogin={onPressLoginHandler}
+          onPressForgotPassword={onPressForgotPasswordHandler}
+          email=""
+          password=""
+          isLoading={true}
+        />
+      );
+
+      expect(wrapper.getByTestId('LoginForm-Button-login').props.disabled).toBe(true);
     });
   });
 });
