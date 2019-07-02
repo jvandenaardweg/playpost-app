c15dc76317c899f61b00b0547c5c25cdd68e8935
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-02 21:24:02 +0200

    Change plus subscription sku

--- src/constants/in-app-purchase.ts
+++ src/constants/in-app-purchase.ts
@@ -1,6 +1,6 @@
 import { Platform } from 'react-native';
 
 export const SUBSCRIPTION_PRODUCT_IDS = Platform.select({
-  ios: ['com.aardwegmedia.playpost.premium', 'com.aardwegmedia.playpost.plus'],
+  ios: ['com.aardwegmedia.playpost.premium', 'com.aardwegmedia.playpost.subscription.plus'],
   android: ['test.sub1', 'test.sub2']
 });
