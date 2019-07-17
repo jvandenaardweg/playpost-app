681520444e4229bf2067466903b78d425e93d028
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-17 16:19:20 +0200

    Localized number formatting in usage component

--- src/components/Usage/index.tsx
+++ src/components/Usage/index.tsx
@@ -5,6 +5,7 @@ import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
 import styles from './styles';
 
+
 interface Props {
   user: Api.User | null;
   activeSubscriptionProductId: string;
@@ -19,6 +20,9 @@ export const Usage: React.FC<Props> = React.memo(({ user, activeSubscriptionProd
   const usageUsedCurrentMonthInSeconds = user.used.audiofiles && user.used.audiofiles.currentMonthInSeconds;
   const percentageUsedCurrentMonth = (usageUsedCurrentMonthInSeconds / limitSecondsPerMonth) * 100;
   const percentageUsed = percentageUsedCurrentMonth >= 100 ? 100 : percentageUsedCurrentMonth;
+
+  const currentUsageLocalized = Math.ceil(usageUsedCurrentMonthInSeconds).toLocaleString('nl-NL'); // So we have 5.000 (with a dot)
+  const currentLimitLocalized = Math.ceil(limitSecondsPerMonth).toLocaleString('nl-NL'); // So we have 5.000 (with a dot)
 
   const showUpgradeButton = activeSubscriptionProductId === 'free' || activeSubscriptionProductId === 'com.aardwegmedia.playpost.premium';
 
@@ -36,30 +40,27 @@ export const Usage: React.FC<Props> = React.memo(({ user, activeSubscriptionProd
       ? 'com.aardwegmedia.playpost.premium'
       : 'com.aardwegmedia.playpost.premium';
 
+
+
   return (
     <View style={styles.container}>
       <View style={styles.wrapper}>
-        {/* <View style={styles.header}>
-          <Text style={styles.headerTitle}>Usage ({activeSubscriptionName})</Text>
-        </View> */}
         <View style={styles.statsContainer}>
-          {/* <Text style={styles.statsTitle}>Article Audio Minutes</Text> */}
           <View style={styles.statsWrapper}>
             <View>
-              <Text style={styles.statsBigNumber}>{Math.floor(usageUsedCurrentMonthInSeconds)}</Text>
+              <Text style={styles.statsBigNumber}>{currentUsageLocalized}</Text>
             </View>
             <View style={styles.statsNumbersContainer}>
               <View>
-                {/* <Text style={styles.statsMeta}>({Math.ceil(usageAvailableCurrentMonthInSeconds)} minutes available)</Text> */}
-                <Text style={styles.statsMeta}>of {Math.ceil(limitSecondsPerMonth)} minutes used</Text>
+                <Text style={styles.statsMeta}>of {currentLimitLocalized} minutes used</Text>
               </View>
               <View>
                 <Text style={styles.statsPercentage}>{Math.ceil(percentageUsed)}%</Text>
               </View>
             </View>
           </View>
-          <View style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)', height: 8, borderRadius: 8, overflow: 'hidden', marginTop: 6 }}>
-            <View style={{ backgroundColor: colors.white, height: 8, borderRadius: 8, width: `${percentageUsed}%` }} />
+          <View style={styles.progressContainer}>
+            <View style={[styles.progress, { width: `${percentageUsed}%` }]} />
           </View>
         </View>
 
