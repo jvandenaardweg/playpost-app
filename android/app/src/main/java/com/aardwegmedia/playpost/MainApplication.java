3af27a65491798400d74e97c631de633584989aa
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-18 14:56:09 +0200

    Added multidex for build error

--- android/app/src/main/java/com/aardwegmedia/playpost/MainApplication.java
+++ android/app/src/main/java/com/aardwegmedia/playpost/MainApplication.java
@@ -1,6 +1,7 @@
 package com.aardwegmedia.playpost;
 
-import android.app.Application;
+// import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.facebook.react.ReactApplication;
 import com.swmansion.rnscreens.RNScreensPackage;
@@ -32,7 +33,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+public class MainApplication extends MultiDexApplication implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
