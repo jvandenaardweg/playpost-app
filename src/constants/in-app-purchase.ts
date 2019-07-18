ee5d260ac46ae0d5492107425d4873b040fd1b92
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-18 19:40:25 +0200

    Added Android subscription productIds

--- src/constants/in-app-purchase.ts
+++ src/constants/in-app-purchase.ts
@@ -1,6 +1,7 @@
 import { Platform } from 'react-native';
 
 export const SUBSCRIPTION_PRODUCT_IDS = Platform.select({
+  // We keep the productId's the same between Apple and Google
   ios: ['com.aardwegmedia.playpost.premium', 'com.aardwegmedia.playpost.subscription.plus'],
-  android: ['test.sub1', 'test.sub2']
+  android: ['com.aardwegmedia.playpost.premium', 'com.aardwegmedia.playpost.subscription.plus']
 });
