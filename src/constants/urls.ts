dc4ff3b66abfdfe88dc023d864ba6135a8c1adaf
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-22 10:22:55 +0200

    Changed URLs to latest path

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,5 +1,5 @@
-export const URL_PRIVACY_POLICY = 'https://playpost.app/privacy-policy';
-export const URL_TERMS_OF_USE = 'https://playpost.app/terms-of-use';
+export const URL_PRIVACY_POLICY = 'https://playpost.app/privacy';
+export const URL_TERMS_OF_USE = 'https://playpost.app/terms';
 export const URL_ABOUT = 'https://playpost.app';
 export const URL_FEEDBACK = 'https://playpost.app/support';
 export const URL_MANAGE_APPLE_SUBSCRIPTIONS = 'https://buy.itunes.apple.com/WebObjects/MZFinance.woa/wa/manageSubscriptions';
