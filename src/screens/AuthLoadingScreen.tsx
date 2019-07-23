d817de74ba214f8467e1dea6a721a14e28a17b59
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-07-23 14:51:45 +0200

    Catch error on AuthLoadingScreen

--- src/screens/AuthLoadingScreen.tsx
+++ src/screens/AuthLoadingScreen.tsx
@@ -1,15 +1,16 @@
 import AsyncStorage from '@react-native-community/async-storage';
 import NetInfo from '@react-native-community/netinfo';
+import Analytics from 'appcenter-analytics';
 import React from 'react';
+import { Alert, Platform, NativeModules } from 'react-native';
 import RNFS from 'react-native-fs';
 import * as Keychain from 'react-native-keychain';
+import SplashScreen from 'react-native-splash-screen';
 import { NavigationRoute, NavigationScreenProp } from 'react-navigation';
 
 import { persistor } from '../store';
 
 import { LOCAL_CACHE_AUDIOFILES_PATH, LOCAL_CACHE_VOICE_PREVIEWS_PATH } from '../constants/files';
-
-import { Platform } from 'react-native';
 
 export const keychainArguments = Platform.select({
   ios: { accessGroup: 'group.playpost', service: 'com.aardwegmedia.playpost' },
@@ -27,52 +28,58 @@ export class AuthLoadingScreen extends React.PureComponent<Props> {
 
   // Upon load of the app, do the following...
   public bootstrapAsync = async () => {
+    try {
+      // Determine if this is the first run after install
+      // If so, delete any API token we had from a previous install
+      // This will make sure the user starts clean and will see the onboarding screen, instead of the app screen
+      const hasRunBefore = await AsyncStorage.getItem('@hasRunBefore');
 
-    // Determine if this is the first run after install
-    // If so, delete any API token we had from a previous install
-    // This will make sure the user starts clean and will see the onboarding screen, instead of the app screen
-    const hasRunBefore = await AsyncStorage.getItem('@hasRunBefore');
+      if (!hasRunBefore) {
+        // Delete any API token we had from a previous install
+        await Keychain.resetGenericPassword(keychainArguments);
 
-    if (!hasRunBefore) {
-      // Delete any API token we had from a previous install
-      await Keychain.resetGenericPassword(keychainArguments);
+        // Reset the persisted store, so we start clean from previous installs
+        await persistor.purge();
 
-      // Reset the persisted store, so we start clean from previous installs
-      await persistor.purge();
+        // Set the item to something, so we don't reset the API token upon next launch
+        await AsyncStorage.setItem('@hasRunBefore', 'true');
+      }
 
-      // Set the item to something, so we don't reset the API token upon next launch
-      await AsyncStorage.setItem('@hasRunBefore', 'true');
+      // Check if the user already has an API token
+      // Important: Only rely on this token, so the user can use the app offline
+      const credentials = await Keychain.getGenericPassword(keychainArguments);
+      let token = null;
+
+      if (credentials && credentials.password) {
+        token = credentials.password;
+      }
+
+      // Get the network connection status
+      // So we know if we can do API requests, or not
+      await NetInfo.isConnected.fetch();
+
+      // Make sure we have the correct cache direct\ories needed in the app
+      await RNFS.mkdir(LOCAL_CACHE_AUDIOFILES_PATH);
+      await RNFS.mkdir(LOCAL_CACHE_VOICE_PREVIEWS_PATH);
+
+      if (!token) {
+        return this.props.navigation.navigate('Onboarding');
+      }
+
+      // User is logged in if we end up here
+
+      // Prepare the app for the logged in user
+
+      // Pre-populate the app with the user data
+      // Every other pre-population is done within the start screen, so we can load faster
+      // await store.dispatch(getUser());
+      return this.props.navigation.navigate('App');
+    } catch (err) {
+      SplashScreen.hide();
+      // Catch any error that might happen during startup
+      Analytics.trackEvent('Startup error', { Error: `${err}`, Message: `${err.message}` });
+      return Alert.alert('Oops!', err.message);
     }
-
-    // Check if the user already has an API token
-    // Important: Only rely on this token, so the user can use the app offline
-    const credentials = await Keychain.getGenericPassword(keychainArguments);
-    let token = null;
-
-    if (credentials && credentials.password) {
-      token = credentials.password;
-    }
-
-    // Get the network connection status
-    // So we know if we can do API requests, or not
-    await NetInfo.isConnected.fetch();
-
-    // Make sure we have the correct cache direct\ories needed in the app
-    await RNFS.mkdir(LOCAL_CACHE_AUDIOFILES_PATH);
-    await RNFS.mkdir(LOCAL_CACHE_VOICE_PREVIEWS_PATH);
-
-    if (!token) {
-      return this.props.navigation.navigate('Onboarding');
-    }
-
-    // User is logged in if we end up here
-
-    // Prepare the app for the logged in user
-
-    // Pre-populate the app with the user data
-    // Every other pre-population is done within the start screen, so we can load faster
-    // await store.dispatch(getUser());
-    return this.props.navigation.navigate('App');
   }
 
   // Render any loading content that you like here
