8c68706bf00d651e32be2c143836e7773b12febe
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-08-19 10:23:14 +0200

    Fix problem where usage component disappears when offline

--- src/reducers/user.ts
+++ src/reducers/user.ts
@@ -103,7 +103,6 @@ export function userReducer(state = initialState, action: any): UserState {
       return {
         ...state,
         isLoading: false,
-        details: null,
         error: getUserFailMessage
       };
 
