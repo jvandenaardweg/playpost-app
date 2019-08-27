92fb21fb6bd7d9c1bbe52045227463d8919989a2
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-08-27 17:42:22 +0200

    Fix linter errors

--- src/components/Upgrade/styles.tsx
+++ src/components/Upgrade/styles.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Platform } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 
 import colors from '../../constants/colors';
 import fonts from '../../constants/fonts';
