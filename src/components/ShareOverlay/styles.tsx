5b63785279e458cf8863cd95de8c8dd4f2743834
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-08-27 15:14:20 +0200

    Show correct background color on Android share overlay

--- src/components/ShareOverlay/styles.tsx
+++ src/components/ShareOverlay/styles.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { Platform, StyleSheet } from 'react-native';
 import spacing from '../../constants/spacing';
 
 export default StyleSheet.create({
@@ -6,7 +6,7 @@ export default StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: 'rgba(0,0,0,0.7)'
+    backgroundColor: Platform.OS === 'ios' ? 'rgba(0,0,0,0.7)' : 'transparent' // Android has it's own background color which is good enough
   },
   modalContainer: {
     flex: 1,
