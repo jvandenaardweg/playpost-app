46f264f1c729c804ae998cf25a49affda7d9182e
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-09-26 14:47:10 +0200

    iOS/Android: Show 30 minutes introduction when user signs up

--- src/screens/SignupSuccessScreen.tsx
+++ src/screens/SignupSuccessScreen.tsx
@@ -20,7 +20,8 @@ export class SignupSuccessScreen extends React.PureComponent<Props> {
       <EmptyState
         title={'Welcome to Playpost!'}
         description={[
-          'You can now add articles to your playlist from every app on your phone.'
+          'To welcome you as a new Playpost user, your first 30 minutes are using our highest quality voices for free!',
+          'Start by adding articles to your playlist from every app on your phone.'
         ]}
         actionButtonLabel="Go to my playlist"
         actionButtonOnPress={() => this.props.navigation.navigate('Playlist')}
