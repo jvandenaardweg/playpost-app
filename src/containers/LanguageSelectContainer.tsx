2afe3cc66e50a8234b2d7b5561ebcae2fd9c7e80
Jordy van den Aardweg <jordyvandenaardweg@gmail.com>
2019-10-22 13:55:43 +0200

    Rewrite languageselectcontainer to fc component

--- src/containers/LanguageSelectContainer.tsx
+++ src/containers/LanguageSelectContainer.tsx
@@ -1,5 +1,5 @@
 import analytics from '@react-native-firebase/analytics';
-import React from 'react';
+import React, { useEffect } from 'react';
 import isEqual from 'react-fast-compare';
 import { InteractionManager } from 'react-native';
 import { connect } from 'react-redux';
@@ -20,22 +20,16 @@ type Props = NavigationInjectedProps & StateProps & DispatchProps;
 
 const selectSelectedVoiceForLanguageName = makeSelectedVoiceForLanguageName();
 
-export class LanguagesSelectComponent extends React.Component<Props> {
-  componentDidMount(): void {
+export const LanguagesSelectComponent: React.FC<Props> = React.memo((props) => {
+
+  useEffect(() => {
     InteractionManager.runAfterInteractions(() => {
-      this.props.getLanguages();
-      this.props.getUser()
+      props.getLanguages();
+      props.getUser()
     });
-  }
+  }, [])
 
-  shouldComponentUpdate(nextProps: Props): boolean {
-    // Only re-render if props change
-    return !isEqual(this.props, nextProps)
-  }
-
-  keyExtractor = (language: Api.Language, index: number) => index.toString();
-
-  handleOnListItemPress = async (language: Api.Language) => {
+  const handleOnListItemPress = async (language: Api.Language) => {
     NavigationService.navigate('SettingsVoices', { languageName: language.name })
 
     await analytics().logEvent('languages_select', {
@@ -43,7 +37,7 @@ export class LanguagesSelectComponent extends React.Component<Props> {
     });
   }
 
-  getSelectedVoiceSubtitle = (language: Api.Language) => {
+  const getSelectedVoiceSubtitle = (language: Api.Language) => {
     const selectedVoice = selectSelectedVoiceForLanguageName(store.getState(), { languageName: language.name });
 
     if (!selectedVoice) { return ''; }
@@ -54,50 +48,48 @@ export class LanguagesSelectComponent extends React.Component<Props> {
     return `${label} (${selectedVoice.countryCode}) (${genderLabel})`;
   }
 
-  getTotalVoices = (language: Api.Language): number => {
+  const getTotalVoices = (language: Api.Language): number => {
     return language.voices && language.voices.length ? language.voices.length : 0;
   }
 
-  getSectionListItems = (languages: Api.Language[]): IListItem[] => {
+  const getSectionListItems = (languages: Api.Language[]): IListItem[] => {
     return languages.map((language, index) => {
       return {
         key: language.id,
-        subtitle: this.getSelectedVoiceSubtitle(language),
+        subtitle: getSelectedVoiceSubtitle(language),
         title: language.name,
         icon: 'globe',
         iconColor: colors.green,
-        onPress: () => this.handleOnListItemPress(language),
-        value: this.getTotalVoices(language),
+        onPress: () => handleOnListItemPress(language),
+        value: getTotalVoices(language),
         chevron: true
       }
     })
   }
 
-  render() {
-    const { languagesWithActiveVoices } = this.props;
+  const { languagesWithActiveVoices } = props;
 
-    // The first in the array is probably the user his language
-    const userLanguage = languagesWithActiveVoices.slice(0, 1);
+  // The first in the array is probably the user his language
+  const userLanguage = languagesWithActiveVoices.slice(0, 1);
 
-    // The rest are the languages, with the user his language
-    const restLanguages = languagesWithActiveVoices.slice(1, languagesWithActiveVoices.length);
+  // The rest are the languages, with the user his language
+  const restLanguages = languagesWithActiveVoices.slice(1, languagesWithActiveVoices.length);
 
-    const sectionListData = [
-      {
-        key: 'language-user',
-        title: 'Lanuage user',
-        data: this.getSectionListItems(userLanguage),
-      },
-      {
-        key: 'language',
-        title: 'Lanuage',
-        data: this.getSectionListItems(restLanguages)
-      }
-    ];
+  const sectionListData = [
+    {
+      key: 'language-user',
+      title: 'Lanuage user',
+      data: getSectionListItems(userLanguage),
+    },
+    {
+      key: 'language',
+      title: 'Lanuage',
+      data: getSectionListItems(restLanguages)
+    }
+  ];
 
-    return <CustomSectionList sectionListData={sectionListData} />;
-  }
-}
+  return <CustomSectionList sectionListData={sectionListData} />;
+}, isEqual)
 
 interface DispatchProps {
   readonly getLanguages: typeof getLanguages;
